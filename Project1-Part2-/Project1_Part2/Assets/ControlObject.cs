c3e58000ddcb3448705bc850fb00147933c3e1fe
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-15 22:45:18 -0400

    Add empty project

---
+++ Project1-Part2-/Project1_Part2/Assets/ControlObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlObject : MonoBehaviour {
+
+	bool readyToPrint;
+
+	// Use this for initialization
+	void Start () {
+		print ("the game has begun!");
+
+		readyToPrint = true;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= 3 && readyToPrint) {
+			print ("It has been three seconds!");
+			readyToPrint = false;
+		}
+	}
+}
